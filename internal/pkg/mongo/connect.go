0116c6d7f95446c43bd97ae0d73004c9c8f3865f
ripls56 <84716344+ripls56@users.noreply.github.com>
2025-03-26 00:26:20 +0300

    fix: wrong gitignore

---
+++ internal/pkg/mongo/connect.go
@@ -0,0 +1,31 @@
+﻿package mongo
+
+import (
+	"context"
+	"github.com/ripls56/vsservice/internal/pkg/config"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+	"go.mongodb.org/mongo-driver/v2/mongo/options"
+	"os"
+)
+
+func New(cfg config.Config) *mongo.Client {
+	file, err := os.ReadFile(cfg.MongoUrlFile)
+	if err != nil {
+		panic(err)
+	}
+
+	client, err := mongo.Connect(options.Client().ApplyURI(string(file)))
+	if err != nil {
+		panic(err)
+	}
+
+	err = client.Ping(context.Background(), nil)
+	if err != nil {
+		panic(err)
+	}
+	return client
+}
+
+func NewDatabase(c *mongo.Client) *mongo.Database {
+	return c.Database("lsp")
+}
