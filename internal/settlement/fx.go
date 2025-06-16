3d9907ff196877d634f13f1d37c88bf4d99b9be9
ripls56 <84716344+ripls56@users.noreply.github.com>
2025-06-16 16:17:04 +0300

    hotfix

--- internal/settlement/fx.go
+++ internal/settlement/fx.go
@@ -1,10 +1,18 @@
 package settlement
 
 import (
+	"context"
+
 	settlementv1 "github.com/lasthearth/vsservice/gen/settlement/v1"
 	"github.com/lasthearth/vsservice/internal/pkg/logger"
+	"github.com/lasthearth/vsservice/internal/pkg/storage"
 	"github.com/lasthearth/vsservice/internal/server/interceptor"
+<<<<<<< Updated upstream
 	mongorepo "github.com/lasthearth/vsservice/internal/settlement/internal/repository/mongo"
+=======
+	repository "github.com/lasthearth/vsservice/internal/settlement/internal/repository/mongo"
+	"github.com/lasthearth/vsservice/internal/settlement/internal/repository/mongo/repomapper"
+>>>>>>> Stashed changes
 	"github.com/lasthearth/vsservice/internal/settlement/internal/service"
 	"go.uber.org/fx"
 )
@@ -23,7 +31,27 @@ var App = fx.Options(
 		fx.Provide(
 			fx.Private,
 			fx.Annotate(
+<<<<<<< Updated upstream
 				mongorepo.New,
+=======
+				func() *repomapper.MapperImpl {
+					return &repomapper.MapperImpl{}
+				},
+				fx.As(new(repository.Mapper)),
+			),
+			fx.Annotate(
+				func() *sermapper.MapperImpl {
+					return &sermapper.MapperImpl{}
+				},
+				fx.As(new(service.Mapper)),
+			),
+			fx.Annotate(
+				storage.New,
+				fx.As(new(service.Storage)),
+			),
+			fx.Annotate(
+				repository.New,
+>>>>>>> Stashed changes
 				fx.As(new(service.SettlementRepository)),
 			),
 		),
@@ -38,5 +66,32 @@ var App = fx.Options(
 				// fx.ResultTags(`group:"scopers"`),
 			),
 		),
+
+		fx.Invoke(func(lc fx.Lifecycle, storage service.Storage) {
+			lc.Append(fx.Hook{
+				OnStart: func(context.Context) error {
+					bucketName := "settlementreq"
+					exists, err := storage.BucketExists(context.Background(), bucketName)
+					if err != nil {
+						return err
+					}
+					if exists {
+						return nil
+					}
+
+					err = storage.CreateBucket(context.Background(), bucketName)
+					if err != nil {
+						return err
+					}
+
+					err = storage.MakeBucketPublic(context.Background(), bucketName)
+					if err != nil {
+						return err
+					}
+					return nil
+				},
+				OnStop: nil,
+			})
+		}),
 	),
 )
