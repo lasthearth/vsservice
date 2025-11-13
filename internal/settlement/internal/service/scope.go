d672424fd4c651cf009bb982eed301d80f9b0a81
ripls56 <84716344+ripls56@users.noreply.github.com>
2025-11-13 19:15:25 +0300

    security(settlement): add scopes for tags management

--- internal/settlement/internal/service/scope.go
+++ internal/settlement/internal/service/scope.go
@@ -5,8 +5,10 @@ import "github.com/lasthearth/vsservice/internal/server/interceptor"
 func (s *Service) Scope() map[interceptor.Method]interceptor.Scope {
 	srvName := "/settlement.v1.SettlementService/"
 	return map[interceptor.Method]interceptor.Scope{
-		interceptor.Method(srvName + "Approve"):     interceptor.Scope("settlements:manage"),
-		interceptor.Method(srvName + "ListPending"): interceptor.Scope("settlements:manage"),
-		interceptor.Method(srvName + "Reject"):      interceptor.Scope("settlements:manage"),
+		interceptor.Method(srvName + "Approve"):                 interceptor.Scope("settlements:manage"),
+		interceptor.Method(srvName + "ListPending"):             interceptor.Scope("settlements:manage"),
+		interceptor.Method(srvName + "Reject"):                  interceptor.Scope("settlements:manage"),
+		interceptor.Method(srvName + "AddTagToSettlement"):      interceptor.Scope("tags:manage"),
+		interceptor.Method(srvName + "RemoveTagFromSettlement"): interceptor.Scope("tags:manage"),
 	}
 }
